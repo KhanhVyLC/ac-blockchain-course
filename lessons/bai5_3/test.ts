83c2c2c9082cd67fe5c15b6189b3d8ebeedc8fc2
Khánh Vy <165674146+KhanhVyLC@users.noreply.github.com>
2026-04-09 12:02:31 +0700

    Update test.ts

--- lessons/bai5_3/test.ts
+++ lessons/bai5_3/test.ts
@@ -1,13 +1,13 @@
 import { ethers } from "ethers";
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.public.blastapi.io");
-
+// Thay đổi dòng này
+const provider = new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/0e7cc5...");
   const abi = [
     "function getCount() public view returns (uint)",
     "function increment() public"
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
+  const contractAddress = "0x6e7c59cdff95BD280B1501bf031D5aa418a31b7a"; // Replace with your contract address
 
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
